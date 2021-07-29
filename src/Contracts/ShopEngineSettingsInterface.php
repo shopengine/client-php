e09b00bc45a8bd366656421a2c57a7c7cc623e2a
Adrian Block <hello@thisisablock.com>
2021-07-29 11:34:10 +0200

    cleanup

--- src/Contracts/ShopEngineSettingsInterface.php
+++ src/Contracts/ShopEngineSettingsInterface.php
@@ -2,11 +2,10 @@
 
 interface ShopEngineSettingsInterface
 {
-    // Return fully qualified URL
     public function getShopEngineServer() : string;
-    
+
     public function getShopEngineShopIdentifier() : string;
-    
+
     public function getShopEngineSecret() : string;
 
     public function settingArray() : array;
